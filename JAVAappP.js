972f0cf67e0a68a5787889fe0de14a849b875b02
alshishani <>
2021-04-14 14:24:00 +0000

    nature

--- JAVAappP.js
+++ JAVAappP.js
@@ -1,12 +1,22 @@
- .
+console.log('my name is mamoun');
+console.log('i live in zarqa');
 
+console.log(111111);
+var myname ='mamoun';
+console.log(myname);
+//this a second time i am doing this
+console.log(3==='3');
+console.log('hello' +' '+'mamoun');
+//alert
+alert('hello how are you');
+var yourage=prompt('enter your age');
+//if statement
+if(yourage>=5){
+  alert('you can see my website');
+}else{alert('sorry you are under age');}
+document.write('hello iam new here');
+var time= prompt('please enter the time');
+if(time> 18){document.write(nice);}
+else if(time> 12){document.write('good');}
 
-
-
-
-
-
-
-
-
-document.write(' dont you agree that nature is beautiful!?');
+else if(time>= 5){document.write('better');}
